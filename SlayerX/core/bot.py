eb4a1e72e8d99d28aea0d01fc913e41757a6add2
IamStrike <165168813+IamStrike@users.noreply.github.com>
2024-05-05 18:17:09 +0530

    bot.py

--- SlayerX/core/bot.py
+++ SlayerX/core/bot.py
@@ -10,7 +10,7 @@ class Slayer(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="ʜᴇʟʟ x ᴍᴜꜱɪᴄ",
+            name="SlayerX",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
